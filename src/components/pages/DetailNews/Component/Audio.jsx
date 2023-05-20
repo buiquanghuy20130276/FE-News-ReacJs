4afdb980962189c574bd5b1e9041aa966865c10e
TranTyPhuQuy <20130377@st.hcmuaf.edu.vn>
2023-05-20 22:52:41 +0700

    axios post data

--- src/components/pages/DetailNews/Component/Audio.jsx
+++ src/components/pages/DetailNews/Component/Audio.jsx
@@ -2,37 +2,70 @@ import React, {useEffect, useRef, useState} from 'react';
 import axios from "axios";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCirclePlay} from "@fortawesome/free-solid-svg-icons";
+import {join} from "crypto-browserify/example/bundle";
 
 function audio(props) {
+    const[audio,setAudio] = useState('');
     console.log(props.text);
-    const[audio,setAudio] = useState(null);
-    const handleClick = () => {
-        let options ={
-            header: {
-                'Content-Type': 'application/json',
-                'token': 'n-73-U7wDiEDEyt6WyqAwZ-RQSSUfz3tr7ba9xi475XtPhbORvBenYIqGr5qcc6o'
-            },
-            data: {
-            "text": "hệ thống tổng hợp tiếng nói trung tâm không gian mạng",
-            "voice": "trinhthiviettrinh",
-            "id": "2",
-            "without_filter": false,
-            "speed": 1.0,
-            "tts_return_option": 2
-            }
-        };
-        axios.post('https://viettelgroup.ai/voice/api/tts/v1/rest/syn',options).then(response => {
-            let audio = response.data.audio_content;
-            let audioElement = new Audio("data:audio/wav;base64,"+audio);
-            audioElement.play();
-            setAudio(audio);
-        }).catch(error => console.log(error));
+    const handleClick = async () => {
+        console.log("get audio")
+        // const config = {
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'token': 'n-73-U7wDiEDEyt6WyqAwZ-RQSSUfz3tr7ba9xi475XtPhbORvBenYIqGr5qcc6o'
+        //     }
+        // };
+        // const data = {
+        //     text: props.text,
+        //     voice: "trinhthiviettrinh",
+        //     id: "2",
+        //     without_filter: false,
+        //     speed: 1.0,
+        //     tts_return_option: 2
+        // };
+        // axios.post('https://viettelgroup.ai/voice/api/tts/v1/rest/syn',{
+        //     text: props.text,
+        //     voice: "trinhthiviettrinh",
+        //     id: "2",
+        //     without_filter: false,
+        //     speed: 1.0,
+        //     tts_return_option: 2
+        // },{
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'token': 'n-73-U7wDiEDEyt6WyqAwZ-RQSSUfz3tr7ba9xi475XtPhbORvBenYIqGr5qcc6o'
+        //     }
+        // })
+        //     .then(response => {
+        //     let audio = response.data;
+        //     // let audioElement = new Audio("data:audio/wav;base64,"+audio);
+        //     //     audio.play();
+        //     setAudio(audio);
+        // }).catch(error => console.log(error));
+        try {
+            const response = await axios.post('https://viettelgroup.ai/voice/api/tts/v1/rest/syn',{
+                text: props.text,
+                voice: "trinhthiviettrinh",
+                id: "2",
+                without_filter: false,
+                speed: 1.0,
+                tts_return_option: 2
+            },{
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'token': 'n-73-U7wDiEDEyt6WyqAwZ-RQSSUfz3tr7ba9xi475XtPhbORvBenYIqGr5qcc6o'
+                    }})
+            setAudio(response.data.async);
+        } catch (error) {
+            console.log(error);
+        }
     };
     return (
         <>
             <div>
                 <button onClick={handleClick}><FontAwesomeIcon icon={faCirclePlay} />text</button>
-                {audio && <audio src={'data:audio/wav;base64,${audio}'} controls />}
+                {/*{audio && <audio src={'data:audio/wav;base64,${audio}'} controls />}*/}
+                <audio src={audio} controls/>
             </div>
         </>
     );
